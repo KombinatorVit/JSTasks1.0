558d0da1e1cbb91be6170c990e8ff63d09b4258e
Vit Onischenko <slonik0775@gmail.com>
2022-07-15 00:19:51 +0300

    Подсчёт количества свойств объекта

--- script.js
+++ script.js
@@ -1,43 +1,18 @@
-// Сумма свойств объекта
-// Есть объект salaries с произвольным количеством свойств, содержащих заработные платы.
+// Подсчёт количества свойств объекта
+// важность: 5
+// Напишите функцию count(obj), которая возвращает количество свойств объекта:
 //
-//     Напишите функцию sumSalaries(salaries), которая возвращает сумму всех зарплат с помощью метода Object.values и цикла for..of.
-//
-//     Если объект salaries пуст, то результат должен быть 0.
-//
-// Например:
-//
-//     let salaries = {
-//         "John": 100,
-//         "Pete": 300,
-//         "Mary": 250
+//     let user = {
+//         name: 'John',
+//         age: 30
 //     };
 //
-// alert( sumSalaries(salaries) ); // 650
+// alert( count(user) ); // 2
+// Постарайтесь сделать код как можно короче.
+//
+//     P.S. Игнорируйте символьные свойства, подсчитывайте только «обычные».
 
-function sumSalaries(salaries) {
 
-    let sum = 0;
-    for (let salary of Object.values(salaries)) {
-        sum += salary;
-    }
-
-    return sum; // 650
+function count(obj){
+    return Object.keys(obj).length;
 }
-
-let salaries = {
-    "John": 100,
-    "Pete": 300,
-    "Mary": 250
-};
-
-alert( sumSalaries(salaries) ); // 650
-
-// Или, как вариант, мы можем получить сумму, используя методы Object.values и reduce:
-
-// reduce перебирает массив значений salaries,
-// складывает их
-// и возвращает результат
-    function sumSalaries(salaries) {
-        return Object.values(salaries).reduce((a, b) => a + b, 0) // 650
-    }
