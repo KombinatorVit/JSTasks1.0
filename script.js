07d1d7d977f67358c058ad577b44eb32a0f03689
Vit Onischenko <slonik0775@gmail.com>
2022-07-14 22:13:30 +0300

    Фильтрация по диапазону

--- script.js
+++ script.js
@@ -1,22 +1,28 @@
-// Переведите текст вида border-left-width в borderLeftWidth
-// Напишите функцию camelize(str), которая преобразует строки вида «my-short-string» в «myShortString».
+// Фильтрация по диапазону
+// Напишите функцию filterRange(arr, a, b), которая принимает массив arr, ищет в нём элементы между a и b и отдаёт массив этих элементов.
 //
-// То есть дефисы удаляются, а все слова после них получают заглавную букву.
+//     Функция должна возвращать новый массив и не изменять исходный.
 //
-//     Примеры:
+//     Например:
 //
-// camelize("background-color") == 'backgroundColor';
-// camelize("list-style-image") == 'listStyleImage';
-// camelize("-webkit-transition") == 'WebkitTransition';
-// P.S. Подсказка: используйте split, чтобы разбить строку на массив символов, потом переделайте всё как нужно и методом join соедините обратно.
+// let arr = [5, 3, 8, 1];
 //
-function camelize(str) {
-    return str
-        .split('-') // разбивает 'my-long-word' на массив ['my', 'long', 'word']
-        .map(
-            // Переводит в верхний регистр первые буквы всех элементом массива за исключением первого
-            // превращает ['my', 'long', 'word'] в ['my', 'Long', 'Word']
-            (word, index) => index == 0 ? word : word[0].toUpperCase() + word.slice(1)
-        )
-        .join(''); // соединяет ['my', 'Long', 'Word'] в 'myLongWord'
+// let filtered = filterRange(arr, 1, 4);
+//
+// alert( filtered ); // 3,1 (совпадающие значения)
+//
+// alert( arr ); // 5,3,8,1 (без изменений)
+
+
+function filterRange(arr, a, b) {
+    // добавлены скобки вокруг выражения для улучшения читабельности
+    return arr.filter(item => (a <= item && item <= b));
 }
+
+let arr = [5, 3, 8, 1];
+
+let filtered = filterRange(arr, 1, 4);
+
+alert( filtered ); // 3,1 (совпадающие значения)
+
+alert( arr ); // 5,3,8,1 (без изменений)
