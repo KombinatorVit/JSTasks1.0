db7f07bb2df958b17911f7d160cda58882e4ec5d
Vit Onischenko <slonik0775@gmail.com>
2022-07-14 20:35:53 +0300

    Подмассив наибольшей суммы

--- script.js
+++ script.js
@@ -1,22 +1,65 @@
-function sumInput(){
-    let arr = [];
+// Подмассив наибольшей суммы
+// На входе массив чисел, например: arr = [1, -2, 3, 4, -9, 6].
+//
+//     Задача: найти непрерывный подмассив в arr, сумма элементов в котором максимальна.
+//
+//     Функция getMaxSubSum(arr) должна возвращать эту сумму.
+//
+//     Например:
+//
+// getMaxSubSum([-1, 2, 3, -9]) == 5 (сумма выделенных элементов)
+// getMaxSubSum([2, -1, 2, 3, -9]) == 6
+// getMaxSubSum([-1, 2, 3, -9, 11]) == 11
+// getMaxSubSum([-2, -1, 1, 2]) == 3
+// getMaxSubSum([100, -9, 2, -3, 5]) == 100
+// getMaxSubSum([1, 2, 3]) == 6 (берём все)
+// Если все элементы отрицательные – ничего не берём(подмассив пустой) и сумма равна «0»:
+//
+// getMaxSubSum([-1, -2, -3]) = 0
+// Попробуйте придумать быстрое решение: O(n2), а лучше за О(n) операций.
 
-    while(true) {
-     let value = prompt('Введите значение', '0')
+// Медленное решение
 
-        if(value === '' || value === null || !isFinite(value)) break;
+function getMaxSubSum(arr) {
+    let maxSum = 0; // если элементов не будет - возвращаем 0
 
-        arr.push(+value);
+    for (let i = 0; i < arr.length; i++) {
+        let sumFixedStart = 0;
+        for (let j = i; j < arr.length; j++) {
+            sumFixedStart += arr[j];
+            maxSum = Math.max(maxSum, sumFixedStart);
+        }
     }
 
-    let sum = 0;
-    for (let ar of arr) {
-sum += ar;
-    }
-
-    return sum;
-
+    return maxSum;
 }
 
+alert( getMaxSubSum([-1, 2, 3, -9]) ); // 5
+alert( getMaxSubSum([-1, 2, 3, -9, 11]) ); // 11
+alert( getMaxSubSum([-2, -1, 1, 2]) ); // 3
+alert( getMaxSubSum([1, 2, 3]) ); // 6
+alert( getMaxSubSum([100, -9, 2, -3, 5]) ); // 100
 
-alert(sumInput())
+
+// Быстрое решение
+
+
+function getMaxSubSum(arr) {
+    let maxSum = 0;
+    let partialSum = 0;
+
+    for (let item of arr) { // для каждого элемента массива
+        partialSum += item; // добавляем значение элемента к partialSum
+        maxSum = Math.max(maxSum, partialSum); // запоминаем максимум на данный момент
+        if (partialSum < 0) partialSum = 0; // ноль если отрицательное
+    }
+
+    return maxSum;
+}
+
+alert( getMaxSubSum([-1, 2, 3, -9]) ); // 5
+alert( getMaxSubSum([-1, 2, 3, -9, 11]) ); // 11
+alert( getMaxSubSum([-2, -1, 1, 2]) ); // 3
+alert( getMaxSubSum([100, -9, 2, -3, 5]) ); // 100
+alert( getMaxSubSum([1, 2, 3]) ); // 6
+alert( getMaxSubSum([-1, -2, -3]) ); // 0
