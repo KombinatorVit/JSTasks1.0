3480e9ea88d5a4b62879e74738698f7339a51023
Vit Onischenko <slonik0775@gmail.com>
2022-07-15 00:40:50 +0300

    Максимальная зарплата

--- script.js
+++ script.js
@@ -1,23 +1,29 @@
-// /Деструктурирующее присваивание
-// У нас есть объект:
+// Максимальная зарплата
+// важность: 5
+// У нас есть объект salaries с зарплатами:
 //
-    let user = {
-        name: "John",
-        years: 30
-    };
-// Напишите деструктурирующее присваивание, которое:
+//     let salaries = {
+//         "John": 100,
+//         "Pete": 300,
+//         "Mary": 250
+//     };
+// Создайте функцию topSalary(salaries), которая возвращает имя самого высокооплачиваемого сотрудника.
 //
-// свойство name присвоит в переменную name.
-//     свойство years присвоит в переменную age.
-//     свойство isAdmin присвоит в переменную isAdmin (false, если нет такого свойства)
-// Пример переменных после вашего присваивания:
-//
-//     let user = { name: "John", years: 30 };
-//
-// // ваш код должен быть с левой стороны:
-let {name, years: age, isAdmin = false} = user;
+//     Если объект salaries пустой, то нужно вернуть null.
+//     Если несколько высокооплачиваемых сотрудников, можно вернуть любого из них.
+//     P.S. Используйте Object.entries и деструктурирование, чтобы перебрать пары ключ/значение.
 
+function topSalary(salaries) {
 
- alert( name ); // John
- alert( age ); // 30
- alert( isAdmin ); // false
+    let max = 0;
+    let maxName = null;
+
+    for(const [name, salary] of Object.entries(salaries)) {
+        if (max < salary) {
+            max = salary;
+            maxName = name;
+        }
+    }
+
+    return maxName;
+}
