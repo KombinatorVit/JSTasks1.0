bfc6c3704500bc86eba05933e46256d3774c456f
Vit Onischenko <slonik0775@gmail.com>
2022-07-14 23:00:27 +0300

    // Отсортировать пользователей по возрасту

--- script.js
+++ script.js
@@ -1,66 +1,36 @@
-// Трансформировать в объекты
+// Отсортировать пользователей по возрасту
 // важность: 5
-// У вас есть массив объектов user, и у каждого из объектов есть name, surname и id.
-//
-//     Напишите код, который создаст ещё один массив объектов с параметрами id и fullName, где fullName – состоит из name и surname.
+// Напишите функцию sortByAge(users), которая принимает массив объектов со свойством age и сортирует их по нему.
 //
 //     Например:
 //
-// let vasya = { name: "Вася", surname: "Пупкин", id: 1 };
-// let petya = { name: "Петя", surname: "Иванов", id: 2 };
-// let masha = { name: "Маша", surname: "Петрова", id: 3 };
+// let vasya = { name: "Вася", age: 25 };
+// let petya = { name: "Петя", age: 30 };
+// let masha = { name: "Маша", age: 28 };
 //
-// let users = [ vasya, petya, masha ];
+// let arr = [ vasya, petya, masha ];
 //
-// let usersMapped = /* ... ваш код ... */
+// sortByAge(arr);
 //
-//     /*
-//     usersMapped = [
-//       { fullName: "Вася Пупкин", id: 1 },
-//       { fullName: "Петя Иванов", id: 2 },
-//       { fullName: "Маша Петрова", id: 3 }
-//     ]
-//     */
-//
-//     alert( usersMapped[0].id ) // 1
-// alert( usersMapped[0].fullName ) // Вася Пупкин
-// Итак, на самом деле вам нужно трансформировать один массив объектов в другой. Попробуйте использовать =>. Это небольшая уловка.
+// // теперь: [vasya, masha, petya]
+// alert(arr[0].name); // Вася
+// alert(arr[1].name); // Маша
+// alert(arr[2].name); // Петя
 
-let vasya = { name: "Вася", surname: "Пупкин", id: 1 };
-let petya = { name: "Петя", surname: "Иванов", id: 2 };
-let masha = { name: "Маша", surname: "Петрова", id: 3 };
 
-let users = [ vasya, petya, masha ];
+function sortByAge(arr) {
+    arr.sort((a, b) => a.age > b.age ? 1 : -1);
+}
 
-let usersMapped = users.map(user => ({
-    fullName: `${user.name} ${user.surname}`,
-    id: user.id
-}));
+let vasya = { name: "Вася", age: 25 };
+let petya = { name: "Петя", age: 30 };
+let masha = { name: "Маша", age: 28 };
 
-/*
-usersMapped = [
-  { fullName: "Вася Пупкин", id: 1 },
-  { fullName: "Петя Иванов", id: 2 },
-  { fullName: "Маша Петрова", id: 3 }
-]
-*/
+let arr = [ vasya, petya, masha ];
 
-alert( usersMapped[0].id ); // 1
-alert( usersMapped[0].fullName ); // Вася Пупкин
-// Обратите внимание, что для стрелочных функций мы должны использовать дополнительные скобки.
-//
-//     Мы не можем написать вот так:
-//
-//     let usersMapped = users.map(user => {
-//         fullName: `${user.name} ${user.surname}`,
-//             id: user.id
-//     });
-// Как мы помним, есть две функции со стрелками: без тела value => expr и с телом value => {...}.
-//
-// Здесь JavaScript будет трактовать { как начало тела функции, а не начало объекта. Чтобы обойти это, нужно заключить их в «нормальные» скобки:
-//
-//     let usersMapped = users.map(user => ({
-//         fullName: `${user.name} ${user.surname}`,
-//         id: user.id
-//     }));
-//     Теперь всё хорошо.
+sortByAge(arr);
+
+// теперь отсортировано: [vasya, masha, petya]
+alert(arr[0].name); // Вася
+alert(arr[1].name); // Маша
+alert(arr[2].name); // Петя
