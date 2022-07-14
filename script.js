c106f8a4193bf8b37dfa8ab3f00931dc17bafaf7
Vit Onischenko <slonik0775@gmail.com>
2022-07-14 22:51:08 +0300

    Создать расширяемый калькулятор

--- script.js
+++ script.js
@@ -1,24 +1,56 @@
-// Скопировать и отсортировать массив
-// У нас есть массив строк arr. Нужно получить отсортированную копию, но оставить arr неизменённым.
+// Создать расширяемый калькулятор
+// важность: 5
+// Создайте функцию конструктор Calculator, которая создаёт «расширяемые» объекты калькулятора.
 //
-//     Создайте функцию copySorted(arr), которая будет возвращать такую копию.
+//     Задание состоит из двух частей.
 //
-//     let arr = ["HTML", "JavaScript", "CSS"];
+//     Во-первых, реализуйте метод calculate(str), который принимает строку типа "1 + 2" в формате «ЧИСЛО оператор ЧИСЛО» (разделено пробелами) и возвращает результат. Метод должен понимать плюс + и минус -.
 //
-// let sorted = copySorted(arr);
+// Пример использования:
 //
-// alert( sorted ); // CSS, HTML, JavaScript
-// alert( arr ); // HTML, JavaScript, CSS (без изменений)
+//     let calc = new Calculator;
+//
+// alert( calc.calculate("3 + 7") ); // 10
+// Затем добавьте метод addMethod(name, func), который добавляет в калькулятор новые операции. Он принимает оператор name и функцию с двумя аргументами func(a,b), которая описывает его.
+//
+//     Например, давайте добавим умножение *, деление / и возведение в степень **:
+//
+// let powerCalc = new Calculator;
+// powerCalc.addMethod("*", (a, b) => a * b);
+// powerCalc.addMethod("/", (a, b) => a / b);
+// powerCalc.addMethod("**", (a, b) => a ** b);
+//
+// let result = powerCalc.calculate("2 ** 3");
+// alert( result ); // 8
+// Для этой задачи не нужны скобки или сложные выражения.
+//     Числа и оператор разделены ровно одним пробелом.
+//     Не лишним будет добавить обработку ошибок.
 
-// Для копирования массива используем slice() и тут же – сортировку:
+//
+// Обратите внимание, как хранятся методы. Они просто добавляются к внутреннему объекту.
+//     Все тесты и числовые преобразования выполняются в методе calculate. В будущем он может быть расширен для поддержки более сложных выражений.
+    function Calculator() {
 
-    function copySorted(arr) {
-        return arr.slice().sort();
+    this.methods = {
+        "-": (a, b) => a - b,
+        "+": (a, b) => a + b
+    };
+
+    this.calculate = function(str) {
+
+        let split = str.split(' '),
+            a = +split[0],
+            op = split[1],
+            b = +split[2]
+
+        if (!this.methods[op] || isNaN(a) || isNaN(b)) {
+            return NaN;
+        }
+
+        return this.methods[op](a, b);
     }
 
-let arr = ["HTML", "JavaScript", "CSS"];
-
-let sorted = copySorted(arr);
-
-alert( sorted );
-alert( arr );
+    this.addMethod = function(name, func) {
+        this.methods[name] = func;
+    };
+}
