39ae9622404eb3a1e246d4ee2e3ae677f5df189e
Vit Onischenko <slonik0775@gmail.com>
2022-07-15 12:23:22 +0300

    Какой день месяца был много дней назад?

--- script.js
+++ script.js
@@ -1,19 +1,37 @@
-// День недели в европейской нумерации
-// важность: 5
-// В Европейских странах неделя начинается с понедельника (день номер 1), затем идёт вторник (номер 2) и так до воскресенья (номер 7). Напишите функцию getLocalDay(date), которая возвращает «европейский» день недели для даты date.
+// Какой день месяца был много дней назад?
+//     важность: 4
+// Создайте функцию getDateAgo(date, days), возвращающую число, которое было days дней назад от даты date.
+//
+//     К примеру, если сегодня двадцатое число, то getDateAgo(new Date(), 1) вернёт девятнадцатое и getDateAgo(new Date(), 2) – восемнадцатое.
+//
+//     Функция должна надёжно работать при значении days=365 и больших значениях:
+//
+//     let date = new Date(2015, 0, 2);
+//
+// alert( getDateAgo(date, 1) ); // 1, (1 Jan 2015)
+// alert( getDateAgo(date, 2) ); // 31, (31 Dec 2014)
+// alert( getDateAgo(date, 365) ); // 2, (2 Jan 2014)
+// P.S. Функция не должна изменять переданный ей объект date.
 
+// Идея проста: нужно вычесть заданное количество дней из date:
+//
+//     function getDateAgo(date, days) {
+//         date.setDate(date.getDate() - days);
+//         return date.getDate();
+//     }
+// …Но функция не должна изменять объект date. Это очень важно, поскольку внешний код, передающий нам объект, не ожидает его изменения.
+//
+//     Это можно осуществить путём клонирования даты:
 
-function getLocalDay(date) {
+    function getDateAgo(date, days) {
+        let dateCopy = new Date(date);
 
-    let day = date.getDay();
-
-    if (day == 0) { // день недели 0 (воскресенье) в европейской нумерации будет 7
-        day = 7;
+        dateCopy.setDate(date.getDate() - days);
+        return dateCopy.getDate();
     }
 
-    return day;
-}
+let date = new Date(2015, 0, 2);
 
-
-let date = new Date(2012, 0, 3);  // 3 января 2012 года
-alert( getLocalDay(date) );       // вторник, нужно показать 2
+alert( getDateAgo(date, 1) ); // 1, (1 Jan 2015)
+alert( getDateAgo(date, 2) ); // 31, (31 Dec 2014)
+alert( getDateAgo(date, 365) ); // 2, (2 Jan 2014)
