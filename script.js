afbe06e6af39c907705f23125f0d472aae06f46f
Vit Onischenko <slonik0775@gmail.com>
2022-07-17 21:34:53 +0300

    Почему не работает return false?

--- script.js
+++ script.js
@@ -1,95 +1,20 @@
-// Раскрывающееся дерево
-// важность: 5
-// Создайте дерево, которое по клику на заголовок скрывает-показывает потомков:
+// Почему не работает return false?
+//     важность: 3
+// Почему в коде ниже return false не работает?
 //
+//     <script>
+//         function handler() {
+//         alert( "..." );
+//         return false;
+//     }
+//     </script>
 //
-//     Требования:
-//
-//         Использовать только один обработчик событий (применить делегирование)
-// Клик вне текста заголовка (на пустом месте) ничего делать не должен.
-//
-//     Решение состоит из двух шагов:
-//
-//     Оборачиваем текст каждого заголовка дерева в элемент <span>. Затем мы можем добавить стили CSS на :hover и обрабатывать клики только на тексте, т.к. ширина элемента <span> в точности совпадает с шириной текста.
-//     Устанавливаем обработчик на корневой узел дерева tree и ловим клики на элементах <span>, содержащих заголовки.
+//     <a href="https://w3.org" onclick="handler()">браузер откроет w3.org</a>
 
-<!DOCTYPE HTML>
-<html>
 
-<head>
-<style>
-.tree span:hover {
-    font-weight: bold;
+function handler(event){
+    alert('...');
+    event.preventDefault()
 }
 
-.tree span {
-    cursor: pointer;
-}
-</style>
-<meta charset="utf-8">
-</head>
-
-<body>
-
-<ul class="tree" id="tree">
-    <li>Животные
-        <ul>
-            <li>Млекопитающие
-                <ul>
-                    <li>Коровы</li>
-                    <li>Ослы</li>
-                    <li>Собаки</li>
-                    <li>Тигры</li>
-                </ul>
-            </li>
-            <li>Другие
-                <ul>
-                    <li>Змеи</li>
-                    <li>Птицы</li>
-                    <li>Ящерицы</li>
-                </ul>
-            </li>
-        </ul>
-    </li>
-    <li>Рыбы
-        <ul>
-            <li>Аквариумные
-                <ul>
-                    <li>Гуппи</li>
-                    <li>Скалярии</li>
-                </ul>
-            </li>
-            <li>Морские
-                <ul>
-                    <li>Морская форель</li>
-                </ul>
-            </li>
-        </ul>
-    </li>
-</ul>
-
-<script>
-    // поместить все текстовые узлы в элемент <span>
-    // он занимает только то место, которое необходимо для текста
-    for (let li of tree.querySelectorAll('li')) {
-    let span = document.createElement('span');
-    li.prepend(span);
-    span.append(span.nextSibling); // поместить текстовый узел внутрь элемента <span>
-}
-
-    //  ловим клики на всём дереве
-    tree.onclick = function(event) {
-
-    if (event.target.tagName != 'SPAN') {
-    return;
-}
-
-    let childrenContainer = event.target.parentNode.querySelector('ul');
-    if (!childrenContainer) return; // нет детей
-
-    childrenContainer.hidden = !childrenContainer.hidden;
-}
-</script>
-
-</body>
-</html>
+//     <a href="https://w3.org" onclick="handler(event)">браузер откроет w3.org</a>
