30fe70a67ad6c825b64a366267b394ddd9cc8d3f
Vit Onischenko <slonik0775@gmail.com>
2022-07-20 22:14:29 +0300

    1urok JS Native

--- script.js
+++ script.js
@@ -1,3 +1,39 @@
-let a = 8, b = 6;
-// Only change code below this line
-[a,b] = [b,a];
+// // state -> copyState -> changeCopyState => use CopyState with changes
+// console.log(typeof [])
+// console.log(typeof {})
+// console.log(typeof (() => {}))
+// console.log(Array.isArray([]))
+
+
+const user = {
+    name: 'Bob',
+    age: 23,
+    isStudent: true
+}
+
+// const user2 = user;
+//
+// user2.name = 'Alex';
+//
+// console.log(user)
+
+
+let user2 = {...user}
+
+// user2.name = user.name
+// user2.age = user.age;
+
+const deepCopyUser = {...user}
+
+const students = [
+    {name: "Alex"},
+    {name: "Bob"},
+    {name: "Donald"},
+    {name: "Ann"},
+]
+
+console.log(students.map(st => st.name ==='Ann' ? {...st, name: 'Joen'}: st))
+
+const arr = [1,2,3]
+
+const [a, b, c] = arr;
