c12771d5ed1d79b2e31eae01fd21a203444a29a2
Vit Onischenko <slonik0775@gmail.com>
2022-07-21 11:52:53 +0300

    Generate Random Whole Numbers within a Range

--- script.js
+++ script.js
@@ -1,39 +1,6 @@
-// // state -> copyState -> changeCopyState => use CopyState with changes
-// console.log(typeof [])
-// console.log(typeof {})
-// console.log(typeof (() => {}))
-// console.log(Array.isArray([]))
+function randomRange(myMin, MyMax){
 
-
-const user = {
-    name: 'Bob',
-    age: 23,
-    isStudent: true
+    return Math.floor(Math.random() * (myMax - myMin + 1) + myMin);
 }
 
-// const user2 = user;
-//
-// user2.name = 'Alex';
-//
-// console.log(user)
-
-
-let user2 = {...user}
-
-// user2.name = user.name
-// user2.age = user.age;
-
-const deepCopyUser = {...user}
-
-const students = [
-    {name: "Alex"},
-    {name: "Bob"},
-    {name: "Donald"},
-    {name: "Ann"},
-]
-
-console.log(students.map(st => st.name ==='Ann' ? {...st, name: 'Joen'}: st))
-
-const arr = [1,2,3]
-
-const [a, b, c] = arr;
+randomRange(5, 77)
