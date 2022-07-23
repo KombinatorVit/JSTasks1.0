f08680bb0e1a4ce6131f48c4b4bc0a0d6fe6c5bf
Vit Onischenko <slonik0775@gmail.com>
2022-07-23 14:05:48 +0300

    Write Concise Object Literal Declarations Using Object Property Shorthand

--- script.js
+++ script.js
@@ -1,18 +1,9 @@
-const result = {
-    success: ["max-length", "no-amd", "prefer-arrow-functions"],
-    failure: ["no-var", "var-on-top", "linebreak"],
-    skipped: ["no-extra-semi", "no-dup-keys"]
-};
-function makeList(arr) {
+const createPerson = (name, age, gender) => {
     // Only change code below this line
-
-    const failureItems = [];
-    for (let element of arr) {
-        failureItems.push(`<li class="text-warning">${element}</li>`);
-    }
-
+    return {
+        name,
+        age,
+        gender
+    };
     // Only change code above this line
-    return failureItems;
-}
-
-const failuresList = makeList(result.failure);
+};
